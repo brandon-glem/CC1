0809c51214e7d1292d54debe3917944118041867
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-17 11:02:04 -0500

    segundo commit

---
+++ cc1.cpp
@@ -0,0 +1,21 @@
+#include <iostream.h>
+
+main()
+
+{int a,b,suma;
+
+cout<<"\n ingrese numero a:";
+
+cin>>a;
+
+cout<<"\n ingrese numero b:";
+
+cin>>b;
+
+suma=a+b;
+
+cout<<"\n la suma de "<<a<<"+"<<b<<" es:"<<suma;
+
+return 0;
+
+}
